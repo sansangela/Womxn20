58471240f59a4087f802417020456b823dd1bdbb
sansangela <sansangela@hotmail.com>
2020-01-18 15:33:07 -0800

    searchBarFinished

--- back-end/controllers/searchBarCtrl.js
+++ back-end/controllers/searchBarCtrl.js
@@ -1,55 +1,49 @@
-// var mysql = require('mysql');
-// var connection = mysql.createConnection({
-// 	host     : '',
-//   	user     : '',
-// 	password : '',
-// 	database: ''
-// });
 const sqlite3 = require('sqlite3').verbose();
+const path = require('path')
+const dbPath = path.resolve(__dirname, '../db/womxn.db')
 
-// open database in memory
-let db = new sqlite3.Database('../db/womxn.db',sqlite3.OPEN_READWRITE, (err) => {
-	if (err) {
-	  return console.error(err.message);
-	}
-	console.log('Connected to the in-memory SQlite database.');
-});
+function filter(req,res,next) {
+	try {
+		// open database in memory
+		let db = new sqlite3.Database(dbPath,sqlite3.OPEN_READWRITE, (err) => {
+			if (err) {
+			return console.error(err.message);
+			}
+			console.log('Connected to the in-memory SQlite database.');
+		});
 
-db.serialize(() => {
-	db.each(`SELECT * FROM user_info WHERE user_keyword = "Basketball";`, (err, row) => {
-	  if (err) {
-		console.error(err.message);
-	  }
-	  console.log(row);
-	});
-});
-   
-// close the database connection
-db.close((err) => {
-if (err) {
-	return console.error(err.message);
+		var keyword = req.query.keyword;
+
+		db.serialize(() => {
+			sql = `SELECT * FROM user_info WHERE user_keyword = '${keyword}';`;
+			db.each(sql, (err, row) => {
+			if (err) {
+				console.error(err.message);
+			}
+			console.log(row);
+			res.status(200).send(row);
+			});
+		});
+
+		// close the database connection
+		db.close((err) => {
+			if (err) {
+				return console.error(err.message);
+			}
+			console.log('Close the database connection.');
+		});
+		} catch (err) {
+			res.status(500).send('SERVER ERROR:' + err);
+				// close the database connection
+			db.close((err) => {
+				if (err) {
+					return console.error(err.message);
+				}
+				console.log('Close the database connection.');
+			});
+		}
 }
-console.log('Close the database connection.');
-});
 
-// connection.connect()
-// connection.end()
+// filter();
 
-// function filter(req,res,next) {
-// 	try {
-
-// 		  var queryParam = req.query;
-// 		  // query
-//   		sql = `SELECT * FROM ride WHERE departure = '${queryParam.departure}' AND destination = '${queryParam.destination}' AND date = '${queryParam.date}';`;
-
-// 		connection.query(sql,function(err,result,fields) {
-// 			if (err) {throw err;}
-// 			res.status(200).send(result);
-// 		});
-// 	} catch (err) {
-// 		res.status(500).send('SERVER ERROR:' + err);
-// 		connection.end();
-// 	}
-// }
-
-// module.exports = {filter}
+module.exports = {filter}
--- back-end/routes/searchBar.js
+++ back-end/routes/searchBar.js
@@ -1,10 +1,15 @@
 var express = require('express');
-// var searchBarCtrl = require('../controllers/searchBarCtrl');
+var searchBarCtrl = require('../controllers/searchBarCtrl');
 var router = express.Router();
 
 router.get('/',function(req,res,next) {
-	// searchBarCtrl.filter(req,res,next);
-	res.send("success!");
+	// change to relative path
+	res.sendfile('/Users/sangela/Desktop/Womxn20/front-end/heygaucho.html');
 });
+
+router.get('/search',function(req,res,next) {
+	searchBarCtrl.filter(req,res,next);
+	// res.send("search");
+})
 
 module.exports = router;
